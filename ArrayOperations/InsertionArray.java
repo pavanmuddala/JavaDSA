c6d69362636414476253adcb4abc9d18c0cd974c
pavanm <pavanmuddala7654@gmail.com>
2024-04-18 23:25:02 +0530

    single dimentional array operations completed

--- ArrayOperations/InsertionArray.java
+++ ArrayOperations/InsertionArray.java
@@ -6,9 +6,22 @@ public class InsertionArray {
     public static void main(String[] args){
         SDA sda=new SDA(5);
         System.out.println(Arrays.toString(sda.arr));
-        sda.insert(2, 0);
+        sda.insert(2, 340);
         sda.insert(3,2);
         sda.insert(2,70);
         sda.insert(24, 30);
+        // System.out.println(sda.arr[0]+"--->first element");
+        // System.out.println(sda.arr[2]+"--->second element");
+        // //Array traversal
+        // sda.traversal();
+        // System.out.println();
+        // //Search for an element in the array
+        // sda.search(340);
+        // sda.search(0);
+
+        //Delete an element from the array
+        System.out.println(sda.arr[2]+" Before Deletion");
+        sda.delete(0);
+        System.out.println(sda.arr[2]+" after deletion");
     }
 }
--- ArrayOperations/SDA.java
+++ ArrayOperations/SDA.java
@@ -22,5 +22,33 @@ public class SDA {
             System.out.println("some exception arised");
         }
     }
+    public void traversal(){
+        try{
+            for(int i=0;i<arr.length;i++){
+                System.out.print(arr[i]+" --> ");
+            }
+        }catch(Exception e){
+            System.out.println("Array no longer exists");
+        }
+    }
+    //Search for an element in the array
+    public void search(int valuToSearch){
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==valuToSearch){
+                System.out.println("Value found at index  --> "+i);
+                return ;
+            }
+        }
+        System.out.println("Value not found in the array...");
+    }
+    //Delete element at particular index
+    public void delete(int indexToBeDeleted){
+        try{
+            arr[indexToBeDeleted]=Integer.MIN_VALUE;
+            System.out.println("The provided index is deleted successfully");
+        }catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("The provided index is not in the array range");
+        }
+    }
     
 }
